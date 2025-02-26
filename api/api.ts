193313a25041855250957019be2d278ddced1859
enterthestream <zaid9010@gmail.com>
2025-02-26 14:04:46 +0000

    feat: Add pagination functionality to api fetching and queries

--- api/api.ts
+++ api/api.ts
@@ -6,9 +6,9 @@ export const vaApi = axios.create({
   baseURL: "https://api.vam.ac.uk/v2/objects",
 });
 
-export function fetchArtworksVA(query: string) {
+export function fetchArtworksVA(query: string, currentPage: number = 1) {
   return vaApi
-    .get(`/search?q=${query}&page_size=10`)
+    .get(`/search?q=${query}&page=${currentPage}&page_size=10`)
     .then(({ data }) => {
       return data;
     })
@@ -27,20 +27,30 @@ export const metApi = axios.create({
   baseURL: "https://collectionapi.metmuseum.org/public/collection/v1",
 });
 
-export async function fetchArtworksMet(query: string) {
+export async function fetchArtworksMet(query: string, currentPage: number = 1) {
   try {
     const {
       data: { objectIDs },
     } = await metApi.get(`/search?q=${query}`);
 
+    if (!objectIDs || objectIDs.length === 0) return [];
+
     if (objectIDs.length > 0) {
+      const startIndex = currentPage * 10 - 10;
+      const endIndex = currentPage * 10;
+
       const objectDetails = await Promise.all(
-        objectIDs.slice(0, 10).map(async (id: number) => {
-          const { data } = await metApi.get(`/objects/${id}`);
-          return data;
+        objectIDs.slice(startIndex, endIndex).map(async (id: number) => {
+          try {
+            const { data } = await metApi.get(`/objects/${id}`);
+            return data;
+          } catch (err) {
+            return null;
+          }
         })
       );
-      return objectDetails;
+      const validObjects = objectDetails.filter((item) => item !== null);
+      return validObjects;
     }
   } catch (err) {
     throw handleAxiosError(err, "MET");
@@ -49,23 +59,27 @@ export async function fetchArtworksMet(query: string) {
 
 // combined artworks
 
-export async function fetchCombinedArtworks(query: string) {
+export async function fetchCombinedArtworks(
+  query: string,
+  currentPage: number = 1
+) {
   try {
     const [vaData, metData] = await Promise.all([
-      fetchArtworksVA(query).catch((err) => {
+      fetchArtworksVA(query, currentPage).catch((err) => {
         return null;
       }),
-      fetchArtworksMet(query).catch((err) => {
-        return null;
+      fetchArtworksMet(query, currentPage).catch((err) => {
+        return [];
       }),
     ]);
-
     const vaArtworks =
       vaData?.records?.map((item: any) => ({
         id: item.systemNumber,
         title: item._primaryTitle || "Untitled",
-        artist: item._primaryMaker || "Unattributed or unknown",
-        image: item._images._primary_thumbnail || null,
+        artist: item._primaryMaker.name || "Unattributed or unknown",
+        image: item._images?._iiif_image_base_url
+          ? `${item._images._iiif_image_base_url}/full/!300,300/0/default.jpg`
+          : null,
         source: "Victoria and Albert Museum",
       })) || [];
 
@@ -77,7 +91,9 @@ export async function fetchCombinedArtworks(query: string) {
         image: item.primaryImageSmall || null,
         source: "The Metropolitan Museum of Art",
       })) || [];
-    return [...vaArtworks, ...metArtworks];
+
+    const results = [...vaArtworks, ...metArtworks];
+    return results;
   } catch (err) {
     throw handleAxiosError(err, "default");
   }
--- components/hooks/useArtworks.ts
+++ components/hooks/useArtworks.ts
@@ -1,9 +1,39 @@
 import { useQuery } from "@tanstack/react-query";
-import { fetchArtworksVA, fetchArtworksMet } from "@/api/api";
+import { fetchCombinedArtworks } from "@/api/api";
+import { useEffect, useState } from "react";
+import { usePagination } from "./usePagination";
 
-export function useArtworksQuery(query: string) {
+export function useArtworksQuery(query: string, currentPage: number = 1) {
   return useQuery({
-    queryKey: ["va_records", query],
-    queryFn: () => fetchArtworksVA(query),
+    queryKey: ["artworks", query, currentPage],
+    queryFn: () => fetchCombinedArtworks(query, currentPage),
   });
+}
+
+export function useArtworks(query: string) {
+  const [data, setData] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const { currentPage, setHasMore } = usePagination();
+
+  const pageSize = 10;
+
+  useEffect(() => {
+    setIsLoading(true);
+    setError(null);
+
+    fetchCombinedArtworks(query, currentPage)
+      .then((artworks) => {
+        setData(artworks);
+        setHasMore(artworks.length > pageSize);
+      })
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [query, currentPage]);
+
+  return { data, isLoading, error };
 }
